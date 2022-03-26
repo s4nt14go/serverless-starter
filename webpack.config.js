ca14f6238c65e1fccb900c0bc4db1c662960a6ca
s4nt14go <santiagopergamo@gmail.com>
2022-03-26 09:24:23 -0300

    fix: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 
@@ -11,19 +9,24 @@ module.exports = {
     extensions: ['.ts', 'tsx'],
   },
   target: 'node',
-  externals: [nodeExternals()],
   module: {
     rules: [
       {
-        test: /\.(tsx?)$/,
+        test: /\.ts$/,
         loader: 'ts-loader',
-        exclude: [
-          [
-            path.resolve(__dirname, 'node_modules'),
-            path.resolve(__dirname, '.serverless'),
-            path.resolve(__dirname, '.webpack'),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              { targets: { node: '14' }, useBuiltIns: 'usage', corejs: 3 },
+            ],
           ],
-        ],
+        },
       },
     ],
   },
